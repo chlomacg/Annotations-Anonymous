c76975f8e764842bb24cd3aeead21033a871e926
Chloe MacGregor <chloemacgregor29@gmail.com>
2026-02-06 14:54:31 -0600

    chore: update lint settings to condense multiple errors for a single unused variable into one warning

--- eslint.config.js
+++ eslint.config.js
@@ -9,15 +9,13 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [tseslint.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
 ]);
