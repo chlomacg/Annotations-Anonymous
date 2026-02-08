41c09a30f76fea336ffd266ed90734f8d9e3cd23
Chloe MacGregor <chloemacgregor29@gmail.com>
2026-02-07 19:03:36 -0600

    fix: round post age down to 0 seconds

--- web/src/components/PostTimestamp.tsx
+++ web/src/components/PostTimestamp.tsx
@@ -2,7 +2,11 @@ import convert from 'convert';
 
 export function PostTimestamp({ time }: { time: Date }) {
   const now = new Date();
-  const diff = convert(now.getTime() - time.getTime(), 'ms').to('best');
+  const diffMs = now.getTime() - time.getTime();
+  const diff = convert(diffMs, 'ms').to('best');
 
-  return <div className="text-sm text-gray-500">{`${diff.quantity.toFixed(0)}${diff.unit.toString()}`}</div>;
+  // We don't use diff.toString(0) because we want no space between the quantity and unit
+  const age = diffMs < 1000 ? '0s' : `${diff.quantity.toFixed(0)}${diff.unit.toString()}`;
+
+  return <div className="text-sm text-gray-500">{age}</div>;
 }
